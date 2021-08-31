9d8e9d4663571dfa9d6f37f79d95a609cf5ab726
vishalvvr <vishalvijayraghavan@gmail.com>
2021-08-31 11:37:11 +0530

    intial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# rhi18nweb
+#### setup
+```
+$ pip3 install -r requirements.txt
+```
+
+#### generate site
+```
+$ python3 app.py
+```
+
+#### dir layout
+- `/template/content.html`: main page
+- `/template/static/`: consist of all css, js, images, font files used in website
+- `/public/`: is the dir created after running app.py this should consist of all static html site files.
+- `data.json`: data/content for your website
---
+++ app.py
@@ -0,0 +1,27 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+import json
+import shutil
+
+with open('data.json') as fobj:
+    site_data = json.load(fobj)
+
+file_loader = FileSystemLoader("template")
+env = Environment(loader=file_loader)
+
+template = env.get_template('content.html').render(site_data)
+
+public_dir = "./public"
+try:
+    #if path already exists, remove it before copying with copytree()
+    if os.path.exists(public_dir):
+        shutil.rmtree(public_dir)
+        shutil.copytree("./template/static", public_dir)
+except Exception as e:
+    print(e)
+
+with open("./public/index.html","w") as fobj:
+    fobj.writelines(template)
+
+
+
