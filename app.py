469b18d38878728f630bb4f47de8b15810c4dd2e
vishalvvr <vishalvijayraghavan@gmail.com>
2021-08-31 11:51:07 +0530

    update public dir creation logic

--- app.py
+++ app.py
@@ -11,17 +11,12 @@ env = Environment(loader=file_loader)
 
 template = env.get_template('content.html').render(site_data)
 
-public_dir = "public"
-try:
-    #if path already exists, remove it before copying with copytree()
-    if os.path.exists(public_dir):
-        shutil.rmtree(public_dir)
-        shutil.copytree("template/static", public_dir)
-except Exception as e:
-    print(e)
+public_dir = os.path.join(os.getcwd(),"public")
 
-with open(public_dir+"/index.html","w") as fobj:
+#if path already exists, remove it before copying with copytree()
+if os.path.exists(public_dir):
+    shutil.rmtree(public_dir)
+shutil.copytree(os.path.join(os.getcwd(),"template/static"), public_dir)
+
+with open(os.path.join(public_dir,"index.html"),"w") as fobj:
     fobj.writelines(template)
-
-
-
